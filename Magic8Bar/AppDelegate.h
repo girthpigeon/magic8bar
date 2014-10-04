62ab6922f111223a62060a261993e9fad13320f6
Zack <pajka@wisc.edu>
2014-10-03 17:54:53 -0700

    Initial Commit

---
+++ Magic8Bar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Magic8Bar
+//
+//  Created by Admin on 10/3/14.
+//  Copyright (c) 2014 grizzchef. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Magic8Bar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Magic8Bar
+//
+//  Created by Admin on 10/3/14.
+//  Copyright (c) 2014 grizzchef. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
